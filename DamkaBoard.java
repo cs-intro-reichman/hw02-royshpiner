4856eefc4f3b35d5c329c0cc83e2526095b70d98
Roy Shpiner <shpiner11@Roys-MacBook-Air.local>
2024-01-01 19:29:58 +0200

    8

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -5,10 +5,12 @@ public class DamkaBoard {
 	public static void main(String[] args) {
 		int n = Integer.parseInt(args[0]);
 		for (int i=1; i<=n; i++){
-			if (i%2==0)
-			System.out.print(" ");
 			for (int j=1; j<=n;j++){
-				System.out.print("* ");
+				if (i%2==0)
+					System.out.print(" *");
+				else	
+					System.out.print("* ");
+
 			}
 			System.out.println();
 		}
